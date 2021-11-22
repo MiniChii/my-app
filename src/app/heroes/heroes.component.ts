5dbf55e56b044a324d3bacd0d81e7a167abe309c
Francisca Palma <f.p.chii@gmail.com>
2021-11-22 15:30:23 -0300

    Parte2 Tutorial: https://devdocs.io/angular~8/tutorial/toh-pt2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -9,12 +10,13 @@ export class HeroesComponent implements OnInit {
 
   constructor() { }
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  currentHero: Hero;
 
   ngOnInit(): void {
   }
 
+  onSelect(seleccionado: Hero){
+    this.currentHero=seleccionado;
+  }
 }
